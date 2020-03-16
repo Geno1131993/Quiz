2d864cb36d3b67646ff4293d7f6ca7472ccd9082
Danielle <ukka.ukka.nuka.nuka.appu.wa@gmail.com>
2020-03-15 22:07:22 -0700

    Added questions to script.js (not functional yet).

--- script.js
+++ script.js
@@ -9,7 +9,25 @@ var questions = [
       choices: ["quotes", "curly brackets", "parentheses", "square brackets"],
       answer: "parentheses"
     },
-    ///etc.
   ];
 
-  console.log(questions);
+
+
+  //Set up questions
+  for(let i = 0; i < questions.length; i++){
+    createQuestion(questions[i], i);
+  }
+
+  $("#questions").append('<input type = "submit" value = "Submit">');
+
+
+
+
+  //Helper function -- add selected question to the page
+  function createQuestion(question, key){
+    $("#questions").append(question.title + "<br>");
+    for(let i = 0; i < question.choices.length; i++){
+        $("#questions").append('<input type = "radio" name = ' + key + '>' + question.choices[i] + '<br>');
+    }
+    $("#questions").append("<br>");
+}
